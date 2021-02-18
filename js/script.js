a68c4561e8fc40a5b4a58209220a7590c6166693
dnbworks <Dooyongnsaakobalderama@gmail.com>
2021-02-18 14:43:17 +0800

    done with the home page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        arrows:true,
+        autoplay:true,
+        speed:1000,
+        dots:true,
+        prevArrow: ".left",
+        nextArrow: ".right",
+        //fade:true
+    });
+});
